365fb67870ea5f35e8eecd64b36d444b1575a3cb
Gary Clayburg <gclaybur@comcast.net>
2019-04-11 15:17:57 -0600

    memory limits for JVMs

--- visualsyncenv.sh
+++ visualsyncenv.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 export POLICY_GROOVY_HOME=src/main/groovy
+export POLICY_ENABLEDSL="true"
+export POLICY_FORCE_RECOMPILE_ENTRY_POINTS="false"
+export POLICY_FORCE_RELOAD_POLICY_ENGINE="false"
+
 echo "setting POLICY HOME to $POLICY_GROOVY_HOME"
