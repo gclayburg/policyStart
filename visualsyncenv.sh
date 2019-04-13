cb94ee46e17e7bcc4b607389500cd6dd141cbdd6
Gary Clayburg <gclaybur@comcast.net>
2019-04-13 12:02:38 -0600

    fix gitpod groovyHome

--- visualsyncenv.sh
+++ visualsyncenv.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export POLICY_GROOVY_HOME=/workspace/policyStart/src/main/groovy
+export POLICY_GROOVY_HOME=src/main/groovy
 export POLICY_ENABLEDSL="true"
 export POLICY_FORCE_RECOMPILE_ENTRY_POINTS="false"
 export POLICY_FORCE_RELOAD_POLICY_ENGINE="false"
