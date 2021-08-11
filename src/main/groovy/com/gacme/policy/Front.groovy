a893022a655f114adfe0f4e72994c429125a5e79
Gary Clayburg <gclaybur@comcast.net>
2021-08-11 09:42:37 -0600

    initial policy scripts

--- src/main/groovy/com/gacme/policy/Front.groovy
+++ src/main/groovy/com/gacme/policy/Front.groovy
@@ -2,11 +2,17 @@ package com.gacme.policy
 
 import com.garyclayburg.attributes.AttributesClass
 import com.garyclayburg.attributes.TargetAttribute
-import com.garyclayburg.persistence.domain.User
+import com.garyclayburg.persistence.domain.MainUser
 @AttributesClass
 class Front {
-    @TargetAttribute(target="FRONTDOOR",attributeName = "email")
-    static String mail(User user) {
+
+    @TargetAttribute(target="FRONTDOOR",attributeName = "userName")
+    static String username(MainUser user) {
         return user.userName
+    }
+
+    @TargetAttribute(target="FRONTDOOR",attributeName = "id")
+    static String id(MainUser user) {
+        return user.id
     }
 }
